ef97f9072daef2a1f487c959c7f97614cccf302b
cd <satyamupadhyayyt@gmail.com>
2026-02-14 18:11:25 +0530

    practice function based questions

---
+++ day3.js
@@ -0,0 +1,57 @@
+// find factorial of a number
+
+// function fact(n){
+//     let fac = 1;
+//     for(let i = 1; i<=n;i++){
+//         fac = fac*i;
+//     }
+//     return fac;
+// }
+
+
+// console.log(fact(120))
+
+// find max of an array
+// function findMax(arr){
+//     let max = 0;
+//     for(let i=0;i<arr.length;i++){
+//         if(arr[i]>max){
+//             max = arr[i];
+//         }
+//     }
+//     return max;
+// }
+
+// let arr = [10,20,30,40,50,100];
+// console.log(findMax(arr))
+
+// find table of a number
+// function findtable(n){
+//     for(let i=1;i<=10;i++){
+//         console.log(n,"*",i,"=",n*i);
+//     }
+// }
+// findtable(20)
+
+//find digits from array
+// arr = [1,3,5,77,18];
+
+// console.log(addDigit(arr))
+// function addDigit(arr){
+//     let digit=0;
+//     for(let i=0;i<arr.length;i++){
+//         digit=digit+arr[i]
+//     }
+//     return digit;
+// }
+
+// find sum of digit
+// function sumOfDigit(n){
+//     let sum = 0;
+//     while(n>0){
+//         sum = sum+n%10;
+//         n = Math.floor(n/10);
+//     }
+//     return sum;
+// }
+// console.log(sumOfDigit(112255))
