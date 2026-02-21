eaf880cdb0b72ecf1a5b97e2bb3ce886a9da84f4
cd <satyamupadhyayyt@gmail.com>
2026-02-21 18:53:01 +0530

    practice js questions

---
+++ day8.js
@@ -0,0 +1,51 @@
+// //Write a program to remove duplicate item from array
+
+// const arr = [1,2,3,4,5,1,2,3,4];
+// const b = [];
+
+// arr.filter((dup)=>{
+//     if(b.indexOf(arr[dup])==-1){
+//         b.push(arr[dup])
+//     }
+// })
+// console.log("removed array",b)
+
+// What will be output of that code
+
+// const firstname = fun();
+// let name = "vivek";
+// function fun(){
+// return `my name is ${name} malviya`
+// }
+// console.log(firstname)
+
+// function mul(a) {
+// return function (b) {
+// return function (c) {
+// return a * b * c;
+// };
+// };
+// }
+// console.log("output with normal function", mul(2)(4)(2));
+
+// const call = (a) => {
+// return (b) => {
+// return (c) => {
+// return a * b * c;
+// };
+// };
+// };
+// console.log("output with arrow function", call(2)(4)(6));
+
+// for (let i = 0; i < 5; i++) {
+// setTimeout(function () {
+// console.log(i);
+// }, i * 1000)
+// }
+
+// for (var i = 0; i < 5; i++) {
+// setTimeout(function () {
+// console.log(i);
+// }, i * 1000);
+// }
+//////op-55555
